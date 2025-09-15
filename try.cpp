71177956f1e2e42279cdc45983f94bacefe22db5
Hecler-afk <tamerlan.tamer526@gmail.com>
2025-09-15 10:27:27 +0600

    Ads

---
+++ try.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(void)
+{
+    float a, b, c;
+    char d;
+    int p = 0;
+
+    if(p == 1);
+        cout << "first number: ";
+        cin >> a;
+
+        cout << "Add (+, -, *, /) ";
+        cin >> d;
+
+        cout << "second number: ";
+        cin >> b;
+
+        if(d == '+') {
+           c = a + b;
+        }
+        else if(d == '-') {
+           c = a - b;
+        }
+        else if(d == '*') {
+           c = a * b;
+        }
+        else if(d == '/') {
+            if (b != 0 )
+                c = a / b;
+            else
+                cout << "\nWRITE CORRETLY";
+        }
+    else {
+        cout << "\nWrite correttly";
+        
+    }
+    cout << "\nResult = " << c << endl;
+    
+}
